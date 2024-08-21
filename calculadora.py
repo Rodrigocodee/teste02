68cc563daeb57f9ac4f0af43fe4c5615ad525d54
rodrigo Ferreira <rf30065@gmail.com>
2024-08-21 20:09:39 -0300

    atualizacao

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,18 @@
 
 while True:
-    
+    operacao= input("escolha qual vai ser operação + - / *:  ")
+
+    if operacao != '+' and operacao != '-' and operacao != '/' and operacao != '*':
+        print('Digite a operação correta')
+        continue
+
+    num_1 = int(input("Digite um numero: "))
+    num_2 = int(input("Digite outro numero: "))
+
+    if operacao == '+':
+        resultado =  num_1 + num_2
+        print(f'{num_1} + {num_2} = {resultado}')
+
+    if operacao == '-':
+        resultado = num_1 - num_2
+        print(f'{num_1} - {num_2} = {resultado} ')
